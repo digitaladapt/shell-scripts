60b8c70f3f384d06079d33b581b5d363bc5921dd
Andrew Stowell <andrew@digitaladapt.com>
2020-04-16 19:46:53 +0000

    discord and thermal (pi specific) added, discord needs config setup.

---
+++ discord.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source config.sh
+
+HOOK_ARG="$1"
+MESSAGE="${@:2}"
+HOOK="general"
+HOOK_URL=$DISCORD_GENERAL_HOOK
+
+case $HOOK_ARG in
+    thermal)
+        HOOK="thermal"
+        HOOK_URL=$DISCORD_THERMAL_HOOK
+        ;;
+    storage)
+        HOOK="storage"
+        HOOK_URL=$DISCORD_STORAGE_HOOK
+        ;;
+esac
+
+curl -s -H "Content-Type: application/json" -X POST -d "{\"content\": \"${MESSAGE}\"}" $HOOK_URL | jq -r '.id'
+
---
+++ thermal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+name=$(</sys/class/thermal/thermal_zone0/type)
+temp=$(</sys/class/thermal/thermal_zone0/temp)
+echo "${name} $((temp/1000)).$((temp%1000))c"
+
