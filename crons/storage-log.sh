4f80e34663a806581fba895386cb12e75743aedd
Andrew Stowell <andrew@digitaladapt.com>
2020-04-23 14:52:30 +0000

    new storage log cron, discord now supports newlines, etc.

---
+++ crons/storage-log.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LOCATION=`dirname "$0"`
+
+source "${LOCATION}/../config.sh"
+
+storage=`${LOCATION}/../df-custom.sh ${STORAGE_MONITOR[@]}`
+
+echo "${storage}"
+${LOCATION}/../discord.sh storage "${storage}"
+
--- discord.sh
+++ discord.sh
@@ -5,7 +5,7 @@ LOCATION=`dirname "$0"`
 source "${LOCATION}/config.sh"
 
 HOOK_ARG="$1"
-MESSAGE="${@:2}"
+MESSAGE=`echo "${@:2}" | jq -aRs .`
 HOOK="general"
 HOOK_URL=$DISCORD_GENERAL_HOOK
 
@@ -20,5 +20,5 @@ case $HOOK_ARG in
         ;;
 esac
 
-curl -s -H "Content-Type: application/json" -X POST -d "{\"username\": \"${DISCORD_SERVER_NAME}\", \"content\": \"${MESSAGE}\"}" $HOOK_URL | jq -r '.id'
+curl -s -H "Content-Type: application/json" -X POST -d "{\"username\": \"${DISCORD_SERVER_NAME}\", \"content\": ${MESSAGE}}" $HOOK_URL | jq -r '.id'
 
