ea20870a0b8f3e4c19f2b72bdc6ba93f7e790131
Andrew Stowell <andrew@digitaladapt.com>
2022-05-06 01:06:19 +0000

    added make/gcc/etc to essential software, since I always end up compiling stuff.

--- one-time-stuff/install-software.sh
+++ one-time-stuff/install-software.sh
@@ -53,7 +53,7 @@ function install_collection () {
 # ----------------------------------------------------------
 
 echo "----- Install core utilities: curl, vim, git, jq.. ---"
-install_collection 'core utilities' curl fail2ban git htop jq grep gzip net-tools goaccess dnsutils bash-completion cron vim
+install_collection 'core utilities' curl fail2ban git htop jq grep gzip net-tools goaccess dnsutils bash-completion cron vim make chrony build-essential gcc
 
 echo "----- Install extra utilities: ncdu, zip, iftop -----"
 install_collection 'extra utilities' ncdu zip unzip iftop
