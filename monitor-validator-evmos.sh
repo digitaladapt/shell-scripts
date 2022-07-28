06399e537a51112fbb1ba26864f76754b66a493d
Andrew Stowell <andrew@digitaladapt.com>
2022-07-28 02:37:04 +0000

    monitor validators, now with logging.

--- monitor-validator-evmos.sh
+++ monitor-validator-evmos.sh
@@ -5,6 +5,13 @@ LOCATION=`dirname "$0"`
 missed_blocks=`curl -s 'https://api-evmos.cosmostation.io/v1/staking/validator/uptime/evmosvaloper187860d5xukeksge9jz3wwff7h68asdafxdxh4r' | jq '.uptime | length'`
 
 if [ "$missed_blocks" -gt 0 ]; then
-    ${LOCATION}/discord.sh general "EVMOS is missing ${missed_blocks} blocks out of the last 100."
+    `${LOCATION}/discord.sh general "EVMOS is missing ${missed_blocks} blocks out of the last 100."`
+fi
+
+echo -n "${missed_blocks},"
+
+if [ -z `date | grep ':00:'` ]; then
+    echo ""
+    date
 fi
 
--- monitor-validator-lum.sh
+++ monitor-validator-lum.sh
@@ -5,6 +5,13 @@ LOCATION=`dirname "$0"`
 missed_blocks=`curl -s 'https://api-lum.cosmostation.io/v1/staking/validator/uptime/lumvaloper1krkmg6f0sjwalkx3nq39yt0upxgys7alme6lps' | jq '.uptime | length'`
 
 if [ "$missed_blocks" -gt 0 ]; then
-    ${LOCATION}/discord.sh general "Lum Network is missing ${missed_blocks} blocks out of the last 100."
+    `${LOCATION}/discord.sh general "Lum Network is missing ${missed_blocks} blocks out of the last 100."`
+fi
+
+echo -n "${missed_blocks},"
+
+if [ -z `date | grep ':00:'` ]; then
+    echo ""
+    date
 fi
 
