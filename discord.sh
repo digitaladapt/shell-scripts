b21bdc9e5998a35fac2177963c5d2df7ef34f686
Andrew Stowell <andrew@digitaladapt.com>
2023-08-15 20:10:59 +0000

    added color support to discord notifications.

--- discord.sh
+++ discord.sh
@@ -5,7 +5,8 @@ LOCATION=`dirname "$0"`
 source "${LOCATION}/config.sh"
 
 HOOK_ARG="$1"
-MESSAGE=`echo "${@:2}" | jq -aRs .`
+# sed: rewrite json string, because "\u001b[m" needs to "\u001b[0m" instead
+MESSAGE=`echo "${@:2}" | jq -aRs . | sed 's/\\\\u001b\[m/\\\\u001b[0m/g'`
 HOOK="general"
 HOOK_URL=$DISCORD_GENERAL_HOOK
 
@@ -23,6 +24,12 @@ case $HOOK_ARG in
         HOOK_URL=$DISCORD_STORAGE_HOOK
         ;;
 esac
+
+# if color detected, update message from "<content>" to "```ansi <content>```", so it gets displayed correctly
+if [[ "$MESSAGE" == *"\\u001b"* ]]; then
+    MESSAGE="\"\`\`\`ansi\n${MESSAGE:1:-1}\n\`\`\`\""
+    # just here to resolve formatting issue in vim "`"
+fi
 
 curl -s -H "Content-Type: application/json" -X POST -d "{\"username\": \"${DISCORD_SERVER_NAME}\", \"content\": ${MESSAGE}}" $HOOK_URL | jq -r '.id'
 
