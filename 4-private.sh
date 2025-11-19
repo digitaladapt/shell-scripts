d554eefbb34b0919c45292c78efac8c93e7d104a
Andrew Stowell <andrew@digitaladapt.com>
2025-11-18 22:05:01 -0500

    simplified command to get assigned ip

--- 4-private.sh
+++ 4-private.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-ip addr show | grep 'global' | grep -oE 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -n 1
+ip -4 addr list scope global | sed -n 's/.*inet \([0-9\.]\+\).*/\1/p' | head -n 1
+
+#ip addr show | grep 'global' | grep -oE 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -n 1
 
