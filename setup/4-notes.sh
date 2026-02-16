32ecd7006572eea939f6688fe6c6b27a143a47b3
Andrew Stowell <andrew@digitaladapt.com>
2026-02-16 16:04:40 -0500

    added tailscale notes

--- setup/4-notes.sh
+++ setup/4-notes.sh
@@ -2,6 +2,9 @@
 
 echo 'Next steps:'
 echo '* get tailscale install script from the website and run it'
+echo 'typically want to disable tailscale-dns on servers, and maybe offer as exit node:'
+echo 'sudo tailscale up --login-server="https://<your-headscale-domain>" --accept-dns=false --advertise-exit-node'
+echo ''
 echo '* setup config.sh for shell-scripts'
 echo '* setup crons for storage and thermal alerts'
 
